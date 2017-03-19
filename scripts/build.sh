f6f43debb2ae2dda37af3596a04eca5fac93f5fe
reno- <contact@rubiano.fr>
2017-03-19 13:13:23 +0100

    release wheel

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,7 +18,10 @@ ls -lisah
 ls -lisah ./3rdparty/
 ls -lisah ./3rdparty/libossia
 ls -lisah ./3rdparty/libossia/build
-zip ./3rdparty/libossia/build/${REPO}_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip ${REPO}_${TRAVIS_TAG}
-zip ./dist/${REPO}_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip ${REPO}_${TRAVIS_TAG}
-ls -lisah
-cd ../
+
+python setup.py sdist
+
+ls -lisah ./dist
+
+echo ${REPO}-${TRAVIS_TAG}_$TRAVIS_OS_NAME
+echo ${REPO}-${TRAVIS_TAG}
