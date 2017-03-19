6af1f626ff49329b114b35b8bec9050fbb063666
reno- <contact@rubiano.fr>
2017-03-19 15:06:36 +0100

    stupid

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -38,11 +38,16 @@ case "${TOXENV}" in
         ;;
 esac
 
-if [[ "${TRAVIS_OS_NAME}" in linux ]]; then
+
+case "$TRAVIS_OS_NAME" in
+  linux)
   CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz"
   mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
   export PATH=${DEPS_DIR}/cmake/bin:${PATH}
-else
+;;
+  osx)
   brew upgrade cmake || brew install cmake
-fi
+;;
+esac
+
 cmake --version
