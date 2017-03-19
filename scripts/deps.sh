9f7016a63ace0df70b7ebbe322d8ce0bc7309faf
reno- <contact@rubiano.fr>
2017-03-19 15:00:17 +0100

    minor

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -38,7 +38,7 @@ case "${TOXENV}" in
         ;;
 esac
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+if [[ "${TRAVIS_OS_NAME}" in linux ]]; then
   CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz"
   mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
   export PATH=${DEPS_DIR}/cmake/bin:${PATH}
