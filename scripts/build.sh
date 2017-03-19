c2d1c6521f1f982e249b5c71777337788750c713
reno- <contact@rubiano.fr>
2017-03-19 14:01:27 +0100

    use latest cmake

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-case "$TRAVIS_OS_NAME" in
-  linux)
-  export CMAKE_BIN=$(readlink -f "$(find cmake/bin -name cmake -type f )")
-  ;;
-  osx)
-  export CMAKE_BIN=$(which cmake)
-  ;;
-esac
-
-echo ''
-echo ''
-echo '-------------------------------------'
-echo ' ----- Explore 3rdParty Folder ------'
-echo ''
-echo ''
-ls -lisah ./3rdParty/
-echo ''
-echo ''
-echo '-------------------------------------'
-echo ' ----- Explore libossia Folder ------'
-echo ''
-echo ''
-ls -lisah ./3rdParty/libossia
-echo ''
-echo ''
-echo '--------------------------------------'
-echo ' ----- INSTALL WITH PIP locally ------'
-echo ''
-echo ''
-
-
-
-
 case "${TOXENV}" in
     py27)
         sudo apt-get install python
