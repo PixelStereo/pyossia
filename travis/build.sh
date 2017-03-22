cc7f00cf947e4e7ea7ed9b74f1759a66a60a0ad3
reno- <contact@rubiano.fr>
2017-03-22 10:37:57 +0100

    workon

--- travis/build.sh
+++ travis/build.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-case "${TOXENV}" in
+echo '------'
+echo $TOXENV
+echo '------'
+case ${TOXENV} in
     py27)
+        echo 'python 2.7'
         sudo apt-get install python
         pip install -ve .
 
         ;;
     py34)
+        echo 'python 3.4'
         sudo apt-get install python3
         pip3 install -ve .
         ;;
     py35)
+        echo 'python 3.5'
         sudo apt-get install python3
         pip3 install -ve .
         ;;
     py36)
+        echo 'python 3.6'
         sudo apt-get install python3
         pip3 install -ve .
         ;;
 esac
 
-echo ''
-echo ''
-echo ''
-echo '-------------------------------------'
-echo ' ----- MAKE THE WHEEL !!!!!!!! ------'
-echo ''
-echo ''
-python setup.py sdist
+#python setup.py sdist
 
 
-ls -lisah ./dist
+#ls -lisah dist
 
-mv ./dist/${REPO}-${TRAVIS_TAG}.tar.gz ./dist/${REPO}-${TRAVIS_TAG}_${TOXENV}_${TRAVIS_OS_NAME}.tar.gz
+#mv dist/${REPO}-${TRAVIS_TAG}.tar.gz ./dist/${REPO}-${TRAVIS_TAG}_${TOXENV}_${TRAVIS_OS_NAME}.tar.gz
 
-ls -lisah ./dist
+#ls -lisah dist
