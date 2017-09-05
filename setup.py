02af18916e628813fb74209eb159944672120c72
reno- <contact@rubiano.fr>
2017-09-05 16:12:12 +0200

    make it simple for now. Don’t build libossia until deployment workflow will be designed

--- setup.py
+++ setup.py
@@ -21,75 +21,6 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 import versioneer
 __version__ = versioneer.get_version()
 
-
-import os
-import re
-import sys
-import platform
-import subprocess
-
-from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
-
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable, 
-                      '-DOSSIA_PD=0',
-                      '-DOSSIA_MAX=0',
-                      '-DOSSIA_QT=0',
-                      '-DOSSIA_PYTHON=1',
-                      '-DOSSIA_NO_QT=0',
-                      '-DOSSIA_EXAMPLES=0',
-                      '-DOSSIA_STATIC=1',
-                      '-DOSSIA_CI=1',
-                      '-DOSSIA_TESTING=0',
-                      '-DOSSIA_C=0',
-                      '-DQt5_DIR=/usr/local/Cellar/qt5/5.9.1/lib/cmake/Qt5']
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j16', ]
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        print(['cmake', ext.sourcedir] + cmake_args)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
-
 setup(
   name = 'pyossia',
   version=__version__,
@@ -114,13 +45,10 @@ setup(
   # simple. Or you can use find_packages().
   packages=find_packages(exclude=['3rdParty', 'examples', 'docs', 'tests']),
   # useless ?? package_data={'ossia_python': ['ossia_python/*']},
-  install_requires=['zeroconf', 'pybind11>=1.7'],
+  install_requires=['zeroconf', 'pybind11>=1.7', 'Cython', 'pyqt5'],
   extras_require={
     'test': ['coverage']
     },
   download_url = 'https://github.com/PixelStereo/pyossia/tarball/' + __version__,
-  ext_package='/usr/local/lib',
-  ext_modules=[CMakeExtension('ossia_python', sourcedir='./3rdParty/libossia')],
-  cmdclass=dict(build_ext=CMakeBuild),
   zip_safe=False,
 )
