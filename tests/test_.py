a1b4a366d3497c59cb4b3f4ae73242eadd3daaa4
reno- <contact@rubiano.fr>
2017-09-06 14:08:14 +0200

    fix tests

--- tests/test_.py
+++ tests/test_.py
@@ -5,14 +5,7 @@ import unittest
 import os, sys
 
 from pyossia import __version__, add_device
-
-test_device = add_device('Ossia Test Device', mode='local', )
-test_device.expose(protocol='oscquery', ws_port=56789, udp_port=3456)
-test_remote_device = add_device('Ossia Test Remote', mode='mirror', target='http://127.0.0.1:5678', udp_port=3456)
-my_int = test_device.add_param('/test/int', datatype='int', domain=[0, 100], clipmode='both', unique=True)
-my_float = test_device.add_param('/test/float', datatype='float', domain=[0, 1], clipmode='low', unique=True)
-my_string = test_device.add_param('/test/string', datatype='string', domain=['uno', 'dos', 'tres'], clipmode=None)
-my_bool = test_device.add_param('/test/bool', datatype='bool')
+from examples.my_device import *
 
 
 def announcement(test):
@@ -31,12 +24,12 @@ class TestAll(unittest.TestCase):
         self.assertAlmostEqual(my_float.value.get_float(), 0.123456)
 
     def test_device(self):
-        self.assertEqual(test_device.__class__.__name__, 'LocalDevice')
-        # QUESTION : How to test the name of the device? Next one is node, but it might be Ossia Test Device
-        # self.assertEqual(test_device.get_root_node().get_address(), 'Ossia Test Device')
-        self.assertEqual(len(test_device.root_node.children()), 1)
-        self.assertEqual(len(test_device.get_nodes()), 1)
-        self.assertEqual(len(test_device.get_params()), 4)
+        self.assertEqual(my_device.__class__.__name__, 'LocalDevice')
+        # TODO : How to test the name of the device? Next one is node, but it might be Ossia Test Device
+        # self.assertEqual(str(my_device.root_node), 'Python Test Device')
+        self.assertEqual(len(my_device.root_node.children()), 1)
+        self.assertEqual(len(my_device.get_nodes()), 2)
+        self.assertEqual(len(my_device.get_parameters()), 7)
 
 
 
