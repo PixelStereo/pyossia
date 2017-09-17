5bff1ecc475e7ea4973b8df38d603a7d3c09e4de
reno- <contact@rubiano.fr>
2017-09-17 16:11:02 +0200

    need to be filled in

--- pyossia/errors.py
+++ pyossia/errors.py
@@ -5,19 +5,3 @@
 All the Exceptions declared by the module are here
 ---
 """
-
-class OssiaTypeError(Exception):
-    """
-    Waiting for an object of class 'expected', but received an object of class 'received'
-    """
-    def __init__(self, expected, received):
-        super(OssiaTypeError, self).__init__()
-        dbg = 'Wait for an {expected} instance object but receive a {received}'
-        print(dbg.format(expected=expected, received=received.__class__))
-
-class OssiaOutputError(Exception):
-	"""
-	There is no output in this project
-	"""
-	def __init__(self):
-		super(OssiaOutputError, self).__init__()
