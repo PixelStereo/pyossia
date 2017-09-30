2d28144a148a816684526366f04471b5a1e472c2
reno- <contact@rubiano.fr>
2017-09-30 19:09:27 +0200

    simpler

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -22,4 +22,4 @@ pip3 install travis-sphinx
 # TODO : DO IT IN SETUP.PY
 wget "https://github.com/OSSIA/libossia/releases/download/v1.0.0-b3/ossia-python-macos.zip"
 unzip "ossia-python-macos.zip"
-mv "ossia-python/ossia_python.cpython-36m-darwin.so" "pyossia/"
+mv "ossia-python/ossia_python.cpython-36m-darwin.so" "pyossia/ossia_python.so"
