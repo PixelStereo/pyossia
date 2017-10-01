7d0c4d4567fb615d736ba3e7b4c27083f73262b9
reno- <contact@rubiano.fr>
2017-10-01 15:27:13 +0200

    no message

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,14 @@ echo ''
 echo ' ----- MAKE THE WHEEL, PLEASE ! ------'
 echo ''
 
-pip3 install -ve .
-python3 setup.py sdist
+pip3 wheel -ve .
+
+echo '------- WHERE IS THE WHEEL ?? ------'
+ls
+echo ''
 
 if [ -n "$TRAVIS_TAG" ]
 then
+	pip3 install ${REPO}-${TRAVIS_TAG}
 	mv ./dist/${REPO}-${TRAVIS_TAG}.tar.gz ./dist/${REPO}-${TRAVIS_TAG}_${TOXENV}_${TRAVIS_OS_NAME}.tar.gz
 fi
