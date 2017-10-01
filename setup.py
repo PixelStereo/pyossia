206fce621631229835f2dbbbf0ec2d348fde5ad0
reno- <contact@rubiano.fr>
2017-10-01 16:47:22 +0200

    enhance tests

--- setup.py
+++ setup.py
@@ -24,17 +24,17 @@ __version__ = versioneer.get_version()
 
 import platform
 if platform.system() == 'Linux':
-  file = 'ossia-python-3.6-linux_x86_64.tar.gz'
-  url = 'https://github.com/OSSIA/libossia/releases/download/deploy_test-06/' + file
+    file = 'ossia-python-3.6-linux_x86_64.tar.gz'
+    url = 'https://github.com/OSSIA/libossia/releases/download/deploy_test-06/' + file
 elif platform.system() == 'Darwin':
-  file = 'ossia-python-3.6-osx.tar.gz'
-  url = 'https://github.com/OSSIA/libossia/releases/download/deploy_test-06/' + file
+    file = 'ossia-python-3.6-osx.tar.gz'
+    url = 'https://github.com/OSSIA/libossia/releases/download/deploy_test-06/' + file
 
 class build(_build):
-  """
-  A build command class that will be invoked during package install.
-  """
-  sub_commands = _build.sub_commands + [('CustomCommands', None)]
+    """
+    A build command class that will be invoked during package install.
+    """
+    sub_commands = _build.sub_commands + [('CustomCommands', None)]
 
 CUSTOM_COMMANDS = [
     ['wget', url],
@@ -46,62 +46,62 @@ CUSTOM_COMMANDS = [
     ]
 
 class CustomCommands(setuptools.Command):
-  """A setuptools Command class able to run arbitrary commands."""
+    """A setuptools Command class able to run arbitrary commands."""
 
-  def initialize_options(self):
-    print()
-    print('INIT')
-    print()
+    def initialize_options(self):
+        print()
+        print('INIT')
+        print()
 
-  def finalize_options(self):
-    print()
-    print('END')
-    print()
+    def finalize_options(self):
+        print()
+        print('END')
+        print()
 
-  def RunCustomCommand(self, command_list):
-    print('-------> Running command: %s' % command_list)
-    p = subprocess.Popen(
-        command_list,
-        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    # Can use communicate(input='y\n'.encode()) if the command run requires
-    # some confirmation.
-    stdout_data, _ = p.communicate()
-    print('Command output: %s' % stdout_data)
-    if p.returncode != 0:
-      raise RuntimeError(
-          'Command %s failed: exit code: %s' % (command_list, p.returncode))
+    def RunCustomCommand(self, command_list):
+        print('-------> Running command: %s' % command_list)
+        p = subprocess.Popen(
+            command_list,
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # Can use communicate(input='y\n'.encode()) if the command run requires
+        # some confirmation.
+        stdout_data, _ = p.communicate()
+        print('Command output: %s' % stdout_data)
+        if p.returncode != 0:
+            raise RuntimeError(
+                'Command %s failed: exit code: %s' % (command_list, p.returncode))
 
-  def run(self):
-    for command in CUSTOM_COMMANDS:
-      self.RunCustomCommand(command)
+    def run(self):
+        for command in CUSTOM_COMMANDS:
+            self.RunCustomCommand(command)
 
 
 setup(
-  name = 'pyossia',
-  version =__version__,
-  description = 'libossia usefull Classes',
-  long_description = long_description,
-  url = 'https://github.com/PixelStereo/pyossia',
-  author = 'Pixel Stereo',
-  author_email = 'contact@pixelstereo.org',
-  license ='GPLv3+',
-  classifiers = [
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Software Development :: Libraries :: Python Modules'
+    name = 'pyossia',
+    version =__version__,
+    description = 'libossia usefull Classes',
+    long_description = long_description,
+    url = 'https://github.com/PixelStereo/pyossia',
+    author = 'Pixel Stereo',
+    author_email = 'contact@pixelstereo.org',
+    license ='GPLv3+',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-  keywords=['creative', 'controls', 'osc', 'oscquery', 'websocket', 'libossia'],
-  packages = find_packages(),
-  zip_safe=False,
-  cmdclass={
-    # Command class instantiated and run during pip install scenarios.
-    'build': build,
-    'CustomCommands': CustomCommands,
+    keywords=['creative', 'controls', 'osc', 'oscquery', 'websocket', 'libossia'],
+    packages = find_packages(),
+    zip_safe=False,
+    cmdclass={
+        # Command class instantiated and run during pip install scenarios.
+        'build': build,
+        'CustomCommands': CustomCommands,
     }
 )
--- tests/test_.py
+++ tests/test_.py
@@ -12,48 +12,69 @@ from pyossia import ossia
 import pyossia
 
 
-def announcement(test):
-    """
-    Simple function to print usefull graphic separator for the log
-    """
-    print(' ')
-    print('---TESTING ' + test + ' -----')
-    print(' ')
-
 class TestAll(unittest.TestCase):
     """
     Main test, it will trigger all the tests
     """
-
-    """
-    # create the OSSIA Device with the name provided
-    # here for test purpose
-    """
+    # create the OSSIA Device
     my_device = ossia.LocalDevice('PyOssia Test Device')
+    # expose this device to the network
+    # First with oscquery protocol
     my_device.expose(protocol='oscquery', listening_port=3456, sending_port=5678, logger=False)
+    # then through OSC
     my_device.expose(protocol='osc', listening_port=9996, sending_port=9997, logger=False)
-    my_int = my_device.add_param('test/value/int', value_type='int', default=66, domain=[-100, 100])
-    my_float = my_device.add_param('test/value/float', value_type='float', default=0.123456789, domain=[-2, 2])
-    my_bool = my_device.add_param('test/value/bool', value_type='bool', default=True)
-    my_string = my_device.add_param('test/value/string', value_type='string', default='Hello world !', domain=['once', 'loop'])
-    my_vec3f = my_device.add_param('test/value/vec3f', value_type='vec3f', default=(0, 0.57, 0.81),  domain=[0, 1])
-    my_list = my_device.add_param('test/value/list', value_type='list', default=[44100, "my_track.wav", 0.6])
-    my_char = my_device.add_param('test/value/char', value_type='char', default=chr(97))
+    # create a bunch of parameters under different nodes
+    my_int = my_device.add_param('test/numeric/int', value_type='int', default=66, domain=[-100, 100])
+    my_float = my_device.add_param('test/numeric/float', value_type='float', default=0.123456789, domain=[-2, 2])
+    my_bool = my_device.add_param('test/special/bool', value_type='bool', default=True)
+    my_string = my_device.add_param('test/misc/string', value_type='string', default='Hello world !', domain=['once', 'loop'])
+    my_vec2f = my_device.add_param('test/numeric/vec2f', value_type='vec2f', default=(0.5, 0.5),  domain=[0, 1])
+    my_vec3f = my_device.add_param('test/numeric/vec3f', value_type='vec3f', default=(0, 0.57, 0.81),  domain=[0, 360])
+    my_vec4f = my_device.add_param('test/numeric/vec4f', value_type='vec4f', default=(0, 146, 207),  domain=[0, 255])
+    my_list = my_device.add_param('test/misc/list', value_type='list', default=[44100, "my_track.wav", 0.6])
+    my_char = my_device.add_param('test/special/char', value_type='char', default=chr(97))
 
     def test_version(self):
         """
         print the actual version of pyossia
         """
-        announcement(pyossia.__version__)
         self.assertEqual(isinstance(pyossia.__version__, str), True)
+
+    def test_int(self):
+        """
+        test a parameter @value_type float
+        """
+        self.assertEqual(self.my_int.value, 0.0)
+        self.assertEqual(self.my_int.value_type, ossia.ValueType.Int)
+        self.my_int.value = 60
+        self.assertAlmostEqual(self.my_int.value, 60)
 
     def test_float(self):
         """
         test a parameter @value_type float
         """
         self.assertEqual(self.my_float.value, 0.0)
-        self.my_float.push_value(0.123456)
+        self.assertEqual(self.my_float.value_type, ossia.ValueType.Float)
+        self.my_float.value = 0.123456
         self.assertAlmostEqual(self.my_float.value, 0.123456)
+
+    def test_string(self):
+        """
+        test a parameter @value_type float
+        """
+        self.assertEqual(self.my_string.value, '')
+        self.assertEqual(self.my_string.value_type, ossia.ValueType.String)
+        self.my_string.value = 'hello world'
+        self.assertEqual(self.my_string.value, 'hello world')
+
+    def test_vec2f(self):
+        """
+        test a parameter @value_type float
+        """
+        self.assertCountEqual(self.my_vec2f.value, [0.0, 0.0])
+        self.assertEqual(self.my_vec2f.value_type, ossia.ValueType.Vec2f)
+        self.my_vec2f.value = [-1, 1]
+        self.assertCountEqual(self.my_vec2f.value, [-1, 1])
 
     def test_device(self):
         """
@@ -68,9 +89,9 @@ class TestAll(unittest.TestCase):
         # How many children this device have?
         self.assertEqual(len(self.my_device.root_node.children()), 1)
         # How many nodes are under this device?
-        self.assertEqual(len(self.my_device.root_node.get_nodes()), 2)
+        self.assertEqual(len(self.my_device.root_node.get_nodes()), 4)
         # How many parameters under this device?
-        self.assertEqual(len(self.my_device.root_node.get_parameters()), 7)
+        self.assertEqual(len(self.my_device.root_node.get_parameters()), 9)
 
 
 if __name__ == '__main__':
