53c3df65e702bec3f1182e24b5165c44f1441057
reno- <contact@rubiano.fr>
2017-10-01 16:07:52 +0200

    Fix CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,3 +14,24 @@ if [ -n "$TRAVIS_TAG" ]
 then
     pip3 install ${REPO}-${TRAVIS_TAG}
 fi
+
+
+case "$BUILD_TYPE" in
+    DOCS)
+		# build the documentation
+		travis-sphinx -s docs/source build
+		travis-sphinx deploy
+    ;;
+    COVERAGE)
+        cd tests
+		# codacy
+		coverage run --omit ../pyossia/_version.py --omit ../versioneer.py  --include ../pyossia/* test_.py
+		coverage xml
+		coverage report -m
+		python-codacy-coverage -r coverage.xml
+		cd ../
+    ;;
+esac
+
+
+
