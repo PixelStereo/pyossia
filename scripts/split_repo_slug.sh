92f9c8669f3b0952262f8cab641eff917eed81b8
reno- <contact@rubiano.fr>
2017-10-10 14:33:20 +0200

    simplify CI before merging in libossia

--- scripts/split_repo_slug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ${TRAVIS_REPO_SLUG} =~ ([^,]+).*"/"([^,]+) ]]; then
-    OWNER=${BASH_REMATCH[1]}
-    REPO=${BASH_REMATCH[2]}
-fi
-echo $OWNER; echo $REPO
-export OWNER
-export REPO
